34bfdcc2ae53b7aa8c799b180ff28271a510235d
kanpan-kot <46046070+kanpan-kot@users.noreply.github.com>
2019-04-23 12:45:46 +0900

    パーティセーブ機能の追加

--- Prediction.java
+++ Prediction.java
@@ -1,6 +1,7 @@
 package kotodaman;
 
 import java.io.*;
+import java.nio.Buffer;
 
 public class Prediction {
     public static void main(String[] args)throws IOException{
@@ -12,13 +13,12 @@ public class Prediction {
         final int next[] = new int[16];
         final int addr[] = new int[16];
         int hand[] = {-1, -1, -1, -1};
-        int count=0,flg=0,turn=0,move=0,turnstartpoint=0,maxlen=0,rest = 0;
+        int count=0,flg=0,turn,move,turnstartpoint=0,rest = 0;
         String str = null;
 
         while(true) {
             if(count==0) {
-                //Set your party.
-                SetParty(party, br, maxlen);
+                ReadPartyFile(br, party);
             }
 
             //Show your party.
@@ -26,7 +26,10 @@ public class Prediction {
 
             //Set your hands.
             if(count==0) {
-                SetHands(hand,str,br);
+                System.out.println("初期手札を左から順番に番号を半角で入力してください．");
+                for(int i=0; i<hand.length; i++) {
+                    SetHands(hand, i, str, br);
+                }
             }
 
             //Show your hands.
@@ -64,16 +67,16 @@ public class Prediction {
                 //Judge order.
                 if(count == selected.length-2) {
                     for(int i=0;i<order.length;i++) {
-                    	for(int j=order.length;j<pop.length;j++) {
+                        for(int j=order.length;j<pop.length;j++) {
                             if(order[i]!=0)
-                                break; 
+                                break;
                             if(selected[i]==pop[j]) {
                                 order[i] = j-i;
-                            	break;
-                    		}
+                                break;
+                            }
                         }
-                    	if(order[i] == 0)
-                    		rest = i;
+                        if(order[i] == 0)
+                            rest = i;
                     }
                     order[rest] = count + 1 - rest;
                     for(int i=0;i<next.length;i++) {
@@ -82,7 +85,7 @@ public class Prediction {
                     System.out.println("\n\n\n乱数列を特定しました．\nここからは使用したコトダマンのみ入力してください．\n何か入力してください．");
                     br.readLine();
                     System.out.println("\n\n\n");
-                    
+
                     //Set addr[].
                     Setaddr(addr,next);
                     flg++;
@@ -97,24 +100,17 @@ public class Prediction {
         }
     }
 
-    static void SetHands(int[] hand, String str , BufferedReader br)throws IOException {
-        System.out.println("初期手札を左から順番に番号を半角で入力してください．");
-        for(int i=0; i<hand.length; i++) {
-            SetHandsContent(hand, i, str, br);
+    private static void SetHands(int[] hand, int i, String str, BufferedReader br)throws IOException {
+        str = br.readLine();
+        if(!(JudgeEntered(Integer.parseInt(str), hand)) && Integer.parseInt(str) >= 0 && Integer.parseInt(str) <= 11) {
+            hand[i] = Integer.parseInt(str);
+        }else {
+            System.out.println("正しい数字を入力してください．\n\n");
+            SetHands(hand, i, str, br);
         }
     }
-    
-    static void SetHandsContent(int[] hand, int i, String str, BufferedReader br)throws IOException {
-    	str = br.readLine();
-    	if(JudgeEntered(Integer.parseInt(str), hand) == false && Integer.parseInt(str) >= 0 && Integer.parseInt(str) <= 11) {
-    		hand[i] = Integer.parseInt(str);
-    	}else {
-    		System.out.println("正しい数字を入力してください．\n\n");
-    		SetHandsContent(hand, i, str, br);
-    	}
-    }
 
-    static void ShowHands(int[] hand, String[] party) {
+    private static void ShowHands(int[] hand, String[] party) {
         System.out.println("・現在の手札");
         for(int i=0;i<hand.length;i++) {
             System.out.print(party[hand[i]] + " ");
@@ -123,10 +119,10 @@ public class Prediction {
         }
     }
 
-    static void EnterSelectedKotodaman(int[] hand, String str, BufferedReader br, int[] selected, int count) throws IOException{
+    private static void EnterSelectedKotodaman(int[] hand, String str, BufferedReader br, int[] selected, int count) throws IOException{
         System.out.println("使ったコトダマンの番号を半角で入力してください．");
         str = br.readLine();
-        if(JudgeEntered(Integer.parseInt(str),hand)==true) {
+        if(JudgeEntered(Integer.parseInt(str),hand)) {
             selected[count%selected.length] = Integer.parseInt(str);
         }else {
             System.out.println("正しい数字を入力してください．\n\n");
@@ -134,10 +130,10 @@ public class Prediction {
         }
     }
 
-    static void EnterPoppedKotodaman(int[] hand, String str, BufferedReader br, int[] pop, int count)throws IOException {
+    private static void EnterPoppedKotodaman(int[] hand, String str, BufferedReader br, int[] pop, int count)throws IOException {
         System.out.println("出てきたコトダマンの番号を半角で入力してください．");
         str = br.readLine();
-        if(JudgeEntered(Integer.parseInt(str),hand) == false) {
+        if(!(JudgeEntered(Integer.parseInt(str),hand))) {
             pop[count%pop.length] = Integer.parseInt(str);
         }else {
             System.out.println("正しい数字を入力してください．\n\n");
@@ -145,7 +141,7 @@ public class Prediction {
         }
     }
 
-    static boolean JudgeEntered(int enter,int hand[]) {
+    private static boolean JudgeEntered(int enter,int hand[]) {
         for(int i=0;i<hand.length;i++) {
             if(enter == hand[i]) {
                 return true;
@@ -154,13 +150,11 @@ public class Prediction {
         return false;
     }
 
-    static void ShowParty(String[] party) {
+    private static void ShowParty(String[] party) {
         for(int i=0;i<party.length;i++) {
             System.out.printf("%-7s", party[i]);
-            if(i<10)
-                System.out.print(": " + i);
-            else
-                System.out.print(":" + i);
+            System.out.print(":");
+            System.out.printf("%2d", i);
             if(i%4==3)
                 System.out.print("\n");
             else
@@ -169,7 +163,7 @@ public class Prediction {
         System.out.print("\n");
     }
 
-    static void Setaddr(int[] addr, int[] next) {
+    private static void Setaddr(int[] addr, int[] next) {
         for(int i=0;i<addr.length;i++) {
             for(int j=0;j<next.length;j++) {
                 if(next[j]==i) {
@@ -180,24 +174,72 @@ public class Prediction {
         }
     }
 
-    static void SetParty(String[] party, BufferedReader br, int maxlen)throws IOException {
+    private static void SetParty(String[] party, BufferedReader br)throws IOException {
         System.out.println("パーティに入っているコトダマンの名前をすべて入力してください．");
         for(int i=0;i<party.length;i++) {
             party[i] = br.readLine();
-            if(party[i].length() > maxlen)
-                maxlen = party[i].length();
         }
-        for(int i = 0;i<party.length;i++) {
-            for(int j=0;j<maxlen-party[i].length();j++) {
-                party[i] = party[i] + "　";
+        SaveParty(br, party);
+    }
+
+    private static void SaveParty(BufferedReader br, String[] party)throws IOException{
+        System.out.print("入力したパーティを保存しておきますか?[Y/n]:");
+        String str = br.readLine();
+        if(str.equals("Y") || str.equals("y")){
+            try {
+                System.out.print("ファイルの名称を決定してください．: ");
+                str = br.readLine();
+                PrintWriter p = new PrintWriter(new BufferedWriter(new FileWriter("ファイルの場所" + str + ".csv", false)));
+
+                // 内容をセットする
+                for(int i = 0; i < party.length; i++){
+                    p.print(party[i]);
+                    p.println();
+                }
+
+                p.close();
+
+                System.out.println("セーブが完了しました。\n次回からファイル名を入力することで使用できます．\n");
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
+        }else if(str.equals("N") || str.equals("n")){
+            System.out.println("パーティを保存しませんでした．\n");
+        }else{
+            System.out.println("\nPlease Enter \"Y\" or \"N\"");
+            SaveParty(br, party);
         }
     }
 
-    static void SwitchHand(int[] hand, int[] selected, int nextkotnum, int count) {
+    private static void SwitchHand(int[] hand, int[] selected, int nextkotnum, int count) {
         for(int i=0;i<hand.length;i++) {
             if(hand[i]==selected[count%selected.length])
                 hand[i] = nextkotnum;
+        }
+    }
+
+    private static void ReadPartyFile(BufferedReader br, String[] party)throws IOException{
+        System.out.print("CSVファイルに保存されたパーティを読み込みますか?[Y/n]: ");
+        String str = br.readLine();
+        if(str.equals("Y") || str.equals("y")){
+            try {
+                System.out.println("\nパーティを保存したcsvファイルの名称を拡張子を付けずに入力してください．");
+                str = br.readLine();
+                BufferedReader bfr = new BufferedReader(new InputStreamReader(new FileInputStream("ファイルの場所" + str + ".csv")));
+
+                for(int i=0;i<party.length;i++){
+                    party[i] = bfr.readLine();
+                }
+            } catch (Exception e) {
+                System.out.println("ファイルが確認できませんでした．\n");
+                SetParty(party, br);
+            }
+        }else if(str.equals("N") || str.equals("n")){
+            //Set your party.
+            SetParty(party, br);
+        }else{
+            System.out.println("\nPlease Enter \"Y\" or \"N\"");
+            ReadPartyFile(br, party);
         }
     }
 }
